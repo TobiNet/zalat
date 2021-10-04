3bf7a3481c3bb19a5a5888a73c7221f7965d14ad
Mr. T <tobiasmro@googlemail.com>
2021-10-04 15:58:07 +0200

    add second display

--- sev.py
+++ sev.py
@@ -3,8 +3,22 @@ import board
 import adafruit_dht
 import I2C_LCD_driver
 
+import sys
+sys.path.insert(0, './lib_oled96')
+from lib_oled96 import ssd1306
+from smbus import SMBus
+from PIL import ImageFont
+
 dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
 lcd = I2C_LCD_driver.lcd()
+
+i2cbus = SMBus(1)
+oled = ssd1306(i2cbus)
+draw = oled.canvas
+FreeSans20 = ImageFont.truetype('./lib_oled96/FreeSans.ttf', 20)
+FreeSans12 = ImageFont.truetype('./lib_oled96/FreeSans.ttf', 12)
+
+
 
 while True:
     try:
@@ -12,13 +26,18 @@ while True:
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
         print(
-            "Temp: {:.1f} C    Humidity: {}% ".format(
+            "Temp: {:.1f}°C    Humidity: {}% ".format(
                 temperature_c, humidity
             )
         )
         lcd.lcd_clear()
         lcd.lcd_display_string("Zevs Becksalat", 1)
         lcd.lcd_display_string("{:.1f} C {}%".format(temperature_c, humidity), 2)
+        oled.cls()
+        oled.display()
+        draw.text((1, 1), "Zevs Beckzalat", font=FreeSans12, fill=1)
+        draw.text((10, 30), "{:.1f}°C {}%".format(temperature_c, humidity), font=FreeSans20, fill=1)
+        oled.display()
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
